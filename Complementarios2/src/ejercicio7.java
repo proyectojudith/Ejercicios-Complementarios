37a67e8b5052878b5640b495d463717c301689bc
Judith <flores.judith.flores@gmail.com>
2021-10-25 12:10:57 -0300

    version 1

--- Complementarios2/src/ejercicio7.java
+++ Complementarios2/src/ejercicio7.java
@@ -1,26 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EJERCICIO7 {
-
-    public static <def> void main(String[] args) {
-
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Ingrese el primer numero");
-        int numero_1 = scan.nextInt();
+        int i = scan.nextInt();
         System.out.println("Ingrese el segundo numero");
         int numero_2 = scan.nextInt();
 
-
-        for (var i = 1; i <numero_2; i++) {
-            var result = "";
-            if (i % 2 == 0) {
-                result += "Fizz";
+        for (i = 1; i < numero_2; i++) {
+            if (i % 2 == 0 && i % 3 == 0) {
+                System.out.print("'FIzzBuzz'");
+            } else if (i % 2 == 0) {
+                System.out.print("'FIzz'");
+            } else if (i % 3 == 0) {
+                System.out.print("'Buzz'");
+            } else {
+                System.out.print(i);
             }
-            if (i % 3 == 0) {
-                result += "Buzz";
-            }
-            boolean resul;
-            System.out.println(result+i);
+            System.out.print(",");
+        }
+            scan.close();
         }
     }
-}
